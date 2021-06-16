d6c2519bde68e5cf2a71078f34a9b68f06196525
Lee424 <tmdghks424@naver.com>
2021-06-17 01:23:05 +0900

    [Add] SNSType & BackgroundColor Enum 추가

---
+++ MemoryTrace/Support/SNSType.swift
@@ -0,0 +1,14 @@
+//
+//  SNSType.swift
+//  MemoryTrace
+//
+//  Created by seunghwan Lee on 2021/05/26.
+//
+
+import Foundation
+
+enum SNSType: String, Codable {
+    case google = "GOOGLE"
+    case kakao = "KAKAO"
+    case apple = "APPLE"
+}
