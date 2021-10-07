231d44e95a4fd7c270230a5aeba567f68ca70fec
Lee424 <tmdghks424@naver.com>
2021-10-07 13:30:46 +0900

    [Update] Home - Rx Code 리팩토링

---
+++ MemoryTrace/Home/View/HomeControllerRx.swift
@@ -0,0 +1,174 @@
+//
+//  HomeControllerRX.swift
+//  MemoryTrace
+//
+//  Created by seunghwan Lee on 2021/06/29.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import Kingfisher
+
+enum BookUpdateType {
+    case add
+    case delete
+    case update
+}
+
+class HomeControllerRx: UIViewController {
+    
+    @IBOutlet weak var noBookLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bookCollectionView: UICollectionView!
+    @IBOutlet weak var plusButton: UIButton!
+    
+    private let disposeBag = DisposeBag()
+    
+    let viewModel = HomeViewModel(profileStorage: ProfileStorage())
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        plusButton.layer.cornerRadius = 8
+        
+        setupNavi()
+        setupCollectionView()
+        bind()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        ImageCache.default.clearMemoryCache()
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    private func setupNavi() {
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        let rightBarButton = UIBarButtonItem(image: UIImage(named: "mypage"), style: .plain, target: self, action: nil)
+        rightBarButton.imageInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 7)
+        navigationItem.rightBarButtonItem = rightBarButton
+        
+        self.navigationController?.navigationBar.tintColor = .white
+        
+        let configuration = UIImage.SymbolConfiguration(weight: .semibold)
+        navigationController?.navigationBar.backIndicatorImage = UIImage(systemName: "chevron.left", withConfiguration: configuration)?.withRenderingMode(.automatic).withAlignmentRectInsets(UIEdgeInsets(top: 0, left: -18, bottom: 0, right: 0))
+        navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(systemName: "chevron.left")?.withRenderingMode(.automatic).withAlignmentRectInsets(UIEdgeInsets(top: 0, left: -18, bottom: 0, right: 0))
+        self.navigationItem.backButtonTitle = ""
+    }
+    
+    private func setupCollectionView() {
+        bookCollectionView.refreshControl = UIRefreshControl()
+        bookCollectionView.refreshControl?.tintColor = .white
+        bookCollectionView.collectionViewLayout = DiaryBookLayout()
+        let diaryBookCell = UINib(nibName: "DiaryBookCell", bundle: nil)
+        self.bookCollectionView.register(diaryBookCell, forCellWithReuseIdentifier: "DiaryBookCell")
+    }
+    
+    private func bind() {
+        self.viewModel.isReceivedFCM
+            .filter({ $0 == true })
+            .bind { [weak self] _ in
+                if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "DiaryListVC") as? DiaryListViewController {
+                    self?.navigationController?.pushViewController(vc, animated: false)
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        self.viewModel.noBooksLabelIsHidden
+            .observe(on: MainScheduler.instance)
+            .bind(to: noBookLabel.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        self.viewModel.error
+            .asDriver(onErrorJustReturn: .unknown)
+            .drive(onNext: { [weak self] error in
+                self?.showToast(message: error.description, position: .bottom)
+            })
+            .disposed(by: disposeBag)
+        
+        self.viewModel.bookList
+            .asDriver(onErrorJustReturn: [])
+            .drive(bookCollectionView.rx.items(cellIdentifier: "DiaryBookCell", cellType: DiaryBookCell.self)) {
+                index, book, cell in
+
+                cell.colorView.backgroundColor = BackgroundColor(rawValue: book.bgColor)?.color
+                cell.titleLabel.text = book.title
+                cell.turnLabel.text = "\(book.nickname) 작성 중"
+                
+                if let stickerImg = book.stickerImg, let imageURL = URL(string: stickerImg) {
+                    cell.coverImageView.kf.setImage(with: imageURL)
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        self.viewModel.userProfile
+            .map({ $0.nickname })
+            .observe(on: MainScheduler.instance)
+            .bind(to: nameLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        Observable
+            .zip(bookCollectionView.rx
+                    .itemSelected, bookCollectionView.rx
+                        .modelSelected(Book.self))
+            .observe(on: MainScheduler.instance)
+            .bind { [weak self] indexPath, book in
+                guard let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "DiaryListVC") as? DiaryListViewController else {return}
+                
+                CurrentBook.shared.book = book
+                self?.navigationController?.pushViewController(vc, animated: true)
+            }
+            .disposed(by: disposeBag)
+        
+        bookCollectionView.rx
+            .willDisplayCell
+            .bind(onNext: { [unowned self] _, indexPath in
+                if indexPath.item == self.viewModel.bookList.value.count - 3 && self.viewModel.isFetching == false && self.viewModel.hasNext {
+                    self.viewModel.currentPage.accept(self.viewModel.currentPage.value + 1)
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        plusButton.rx.tap
+            .bind { [unowned self] _ in
+                if let decoratingVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "DecoratingVC") as? DecoratingViewController {
+                    decoratingVC.modalPresentationStyle = .fullScreen
+                    self.present(decoratingVC, animated: true)
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        bookCollectionView.rx
+            .didEndDecelerating
+            .asDriver()
+            .drive(onNext: { [unowned self] _ in
+                if let isRefreshing = self.bookCollectionView.refreshControl?.isRefreshing, isRefreshing {
+                    self.bookCollectionView.refreshControl?.endRefreshing()
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                        self.viewModel.pullToRefresh.accept(())
+                    }
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        navigationItem.rightBarButtonItem?.rx.tap
+            .bind { [unowned self] _ in
+                if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MypageVC") as? MypageViewController {
+                    vc.profileStorage = self.viewModel.profileStorage
+                    self.navigationController?.pushViewController(vc, animated: true)
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        NotificationCenter.default.rx.notification(NSNotification.Name("updateBooks"))
+            .bind { [unowned self] _ in
+                self.bookCollectionView.scrollToItem(at: IndexPath(item: 0, section: 0), at: .top, animated: false)
+                self.viewModel.pullToRefresh.accept(())
+            }
+            .disposed(by: disposeBag)
+    }
+}
--- MemoryTrace/Home/ViewModel/HomeViewModel.swift
+++ MemoryTrace/Home/ViewModel/HomeViewModel.swift
@@ -1,46 +1,82 @@
 //
-//  BookListViewModel.swift
+//  HomeViewModel2.swift
 //  MemoryTrace
 //
-//  Created by seunghwan Lee on 2021/06/29.
+//  Created by seunghwan Lee on 2021/10/05.
 //
 
 import Foundation
 import RxSwift
+import RxCocoa
 
 class HomeViewModel {
-    let bookStorage: BookStorageType
-    let profileStorage: ProfileStorage
-    let isReceivedFCM = BehaviorSubject<Bool>(value: false)
     
-    init (bookStorage: BookStorageType, profileStorage: ProfileStorage) {
-        self.bookStorage = bookStorage
+    var disposeBag = DisposeBag()
+    let profileStorage: ProfileStorage
+    var isFetching: Bool = false
+    var hasNext: Bool = true
+    
+    // Input
+    let currentPage = BehaviorRelay<Int>(value: 1)
+    let pullToRefresh = BehaviorRelay<Void>(value: ())
+    let isReceivedFCM = BehaviorRelay<Bool>(value: false)
+    
+    // output
+    let bookList = BehaviorRelay<[Book]>(value: [])
+    let noBooksLabelIsHidden = PublishRelay<Bool>()
+    let error = PublishRelay<NetworkError>()
+    
+    init(profileStorage: ProfileStorage) {
         self.profileStorage = profileStorage
+        bind()
+    }
+    
+    private func bind() {
+        pullToRefresh.asObservable()
+            .subscribe { [weak self] _ in
+                self?.currentPage.accept(1)
+            }
+            .disposed(by: disposeBag)
+        
+        currentPage.asObservable()
+            .flatMapLatest { [weak self] (page: Int) -> Observable<[Book]> in
+                self?.fetchBooks(page: page) ?? Observable.just([])
+            }
+            .subscribe(onNext: { [weak self] books in
+                self?.bookList.accept((self?.currentPage.value == 1 ? [] : (self?.bookList.value ?? [])) + books)
+            }, onError: { [weak self] error in
+                self?.error.accept(.unknown)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    func fetchBooks(page:Int = 1) -> Observable<[Book]> {
+        isFetching = true
+        let result = NetworkManager.shared.fetchBookListRx(page: page)
+        
+        return Observable.create { emitter in
+            result
+                .subscribe(onNext: { [weak self] books in
+                    if books.data.curPage == 1 && books.data.bookList.count == 0 {
+                        self?.noBooksLabelIsHidden.accept(false)
+                    } else {
+                        self?.noBooksLabelIsHidden.accept(true)
+                    }
+                    self?.hasNext = books.data.hasNext
+                    self?.isFetching = false
+                    emitter.onNext(books.data.bookList)
+                    emitter.onCompleted()
+                }, onError: { [weak self] error in
+                    self?.bookList.accept([])
+                    self?.error.accept(.network)
+                    self?.noBooksLabelIsHidden.accept(false)
+                    self?.isFetching = false
+                })
+        }
     }
     
     var userProfile: Observable<Profile> {
         return profileStorage.userProfile()
     }
-    
-    var bookList: Observable<[Book]> {
-        return bookStorage.bookList()
-    }
-    
-    func bookListCount() -> Int {
-        return bookStorage.bookListCount()
-    }
-    
-    func isFetchingAvailable() -> Bool {
-        return bookStorage.isFetchingAvailable()
-    }
- 
-    func fetch(indexPath: IndexPath) {
-        if self.bookStorage.isFetchingAvailable() && indexPath.item == self.bookStorage.bookListCount() - 10  {
-            bookStorage.fetchBooks()
-        }
-    }
-    
-    func refresh() {
-        bookStorage.refreshBooks()
-    }
 }
+
